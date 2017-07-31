61315a4bfe96a76a3af058546de177363c38c552
Murad Tanmoy <murad.tanmoy@qvantel.com>
2017-07-31 14:14:18 +0300

    initial version

---
+++ app/KanbanBoardContainer.js
@@ -0,0 +1,61 @@
+
+
+ import react, {Component} from 'react';
+ import KanbanBoard from './KanbanBoard';
+
+
+ const API_URL = 'http://kanbanapi.pro-react.com';
+ const API_HEADERS = {
+'Content-Type': 'application/json',
+  Authorization: 'any-string-you-like'
+};
+
+
+
+ class KanbanBoardContainer {
+
+  constructor(){
+
+
+    this.state = {
+      cards:[],
+    };
+  }
+  addTask(cardId, taskName){
+
+    }
+
+    deleteTask(cardId, taskId, taskIndex){
+
+    }
+
+    toggleTask(cardId, taskId, taskIndex){
+
+    }
+  componentDidMount(){
+      fetch(API_URL+'/cards', {headers: API_HEADERS})
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({cards: responseData});
+      })
+      .catch((error) => {
+        console.log('Error fetching and parsing data', error);
+      });
+    }
+
+  render() {
+    return(
+
+      <KanbanBoard cards= {this.state.cards}
+
+      taskCallbacks={{
+                             toggle: this.toggleTask.bind(this),
+                             delete: this.deleteTask.bind(this),
+                                add: this.addTask.bind(this) }}
+       />
+    )
+  }
+
+ }
+
+ export default KanbanBoardContainer;
